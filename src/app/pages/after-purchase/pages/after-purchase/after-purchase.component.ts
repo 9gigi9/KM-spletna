fc3b58686e47b6d872a5ac67435098ff882b200c
Vid.Miklavcic <vid.miklavcic@gmail.com>
2026-04-10 13:11:55 +0200

    after purchase remove unused stuff

--- src/app/pages/after-purchase/pages/after-purchase/after-purchase.component.ts
+++ src/app/pages/after-purchase/pages/after-purchase/after-purchase.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-after-purchase',
   standalone: true,
-  imports: [],
   templateUrl: './after-purchase.component.html',
-  styleUrl: './after-purchase.component.css'
+  styleUrl: './after-purchase.component.css',
 })
-export class AfterPurchaseComponent {
-
-}
+export class AfterPurchaseComponent {}
